7e5d63810ad7198aa4c635a55317d5dc40817d92
Juris <juris@juro.com>
2025-08-27 17:14:25 +0300

    tune entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
-yq e -i '(.sources[] | select(.title == "github").name) = env(GH_ORG)' settings.yml
-yq e -i '(.sources[] | select(.title == "github").authName) = env(GH_USER)' settings.yml
-yq e -i '(.sources[] | select(.title == "github").password) = env(GH_PASS)' settings.yml
+: "${GH_ORG:?GH_ORG is required}"
+: "${GH_USER:?GH_USER is required}"
+: "${GH_PASS:?GH_PASS is required}"
+: "${SCM_LOCAL_FOLDER:?SCM_LOCAL_FOLDER is required}"
+
+yq e -i '
+  .localFolder = env(SCM_LOCAL_FOLDER)
+  | with(.sources[] | select(.title == "github");
+      .name = env(GH_ORG)
+    | .authName = env(GH_USER)
+  )
+' settings.yml
 
 exec dotnet ScmBackup.dll
